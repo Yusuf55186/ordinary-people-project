75e2f4075365f45a853ebfb6faf725582b42ee20
Yusuf55186 <youseffakharany@gmail.com>
2026-08-16 15:51:22 +0200

    Scene8 for Episode 1 complete

--- src/scenes/episode1/Episode1Scene8.tsx
+++ src/scenes/episode1/Episode1Scene8.tsx
@@ -4,14 +4,39 @@ import { UiCursor } from "../../components/UiCursor";
 import { AdyManCharacter } from "../../adyManCharacter";
 import { SceneMaster } from "../sceneCharacter";
 import { YoutubeFeed } from "../../components/YoutubeFeed";
+import { interpolate, useCurrentFrame } from "remotion";
 export const Episode1Scene8 = () => {
+    const frame = useCurrentFrame();
+    const cursorX = interpolate(
+        frame,
+        [90,150],[880,990],{
+            extrapolateLeft:"clamp",
+            extrapolateRight:"clamp"
+        }
+    )
+    const retreatStrength = interpolate(
+        frame,
+        [45,90] , [1,0],
+        {
+            extrapolateLeft:"clamp",
+            extrapolateRight:"clamp"
+        }
+    )
+    const hesitationStrength = interpolate(
+        frame,
+        [150,175], [0,1],{
+            extrapolateLeft:"clamp",
+            extrapolateRight:"clamp"
+        }
+    )
     return (
-        <YusufDeskShot retreat={1}>
+        <YusufDeskShot retreat={retreatStrength}
+                       hesitation={hesitationStrength}>
         <YoutubeFeed x={645} y={420} scale={0.57} /> 
                 <SceneMaster x={80} y={296} width={250} scale={1} zIndex={1}>
                     <AdyManCharacter {...adyManPose({})} />
                     </SceneMaster>
-                    <UiCursor x={880} y={471} scale={0.65} />
+                    <UiCursor x={cursorX} y={471} scale={0.65} />
                     </YusufDeskShot>
     )
 }
